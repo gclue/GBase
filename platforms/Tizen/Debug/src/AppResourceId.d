d91fa68acc9678b84c8d6a28508f9bc59cfd975f
Takashi Tsuchiya <takashi@gclue.jp>
2013-05-31 14:37:44 +0900

    ゴミ削除

--- platforms/Tizen/Debug/src/AppResourceId.d
+++
@@ -1,4 +0,0 @@
-src/AppResourceId.d: ../src/AppResourceId.cpp \
-  /Users/tsuchiya/Documents/workspace/GCube2/platforms/Tizen/inc/AppResourceId.h
-
-/Users/tsuchiya/Documents/workspace/GCube2/platforms/Tizen/inc/AppResourceId.h:
